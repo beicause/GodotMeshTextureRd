ae93ca97827c08bdb014b1c07d14d830ddfd6dca
LuoZhihao <luo_zhihao_jx@qq.com>
2025-03-02 23:06:07 +0800

    test pr 100264

--- glsl/base_texture.glsl
+++ glsl/base_texture.glsl
@@ -1,11 +1,12 @@
-#[vertex]
 #version 450
+
+#pragma godot_shader_stages(vertex, fragment)
+
+#ifdef GODOT_STAGE_VERTEX
 
 layout(location=0) in vec3 position;
 layout(location=1) in vec2 uv;
-
 layout(location=0) out vec2 frag_uv;
-
 layout(set=0, binding=0) uniform Transform {
     mat4 matrix;
 };
@@ -15,16 +16,16 @@ void main() {
     gl_Position = matrix * vec4(position,1.0);
 }
 
+#endif //GODOT_STAGE_VERTEX
 
-#[fragment]
-#version 450
+#ifdef GODOT_STAGE_FRAGMENT
 
 layout(location=0) in vec2 frag_uv;
-
 layout(location=0) out vec4 frag_color;
-
 layout(set=0, binding=1) uniform sampler2D tex;
 
 void main() {
     frag_color = texture(tex, frag_uv);
 }
+
+#endif //GODOT_STAGE_FRAGMENT
